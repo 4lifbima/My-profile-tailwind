f1812a6e4b7f4923edf290310555a9603d106c1e
4lifbima <alif67916@gmail.com>
2024-09-19 23:55:07 +0800

    Penempatan Project Terbaru

--- src/services/projects.js
+++ src/services/projects.js
@@ -2,10 +2,10 @@ import techStack from "./techStack";
 const projects = [
   {
     id: 1,
-    title: "UI/UX Mobile Apps",
-    name: "Dapur Kreatif",
+    title: "Web",
+    name: "SIMKBS",
     status: "preview",
-    imgPath: "/assets/album/dapurkreatif.png",
+    imgPath: "/assets/album/simkbs.png",
     url: "https://shorturl.at/xEJKT",
     techStack: [techStack.find((tech) => tech.id === 1)].filter(Boolean),
     category: "UI/UX",
@@ -23,12 +23,12 @@ const projects = [
   {
     id: 2,
     title: "Web ",
-    name: "Pesona Flobamora",
+    name: "Silapor Polda",
     status: "comming soon",
-    imgPath: "/assets/album/web-wisata.png",
+    imgPath: "/assets/album/silapor.png",
     url: "",
     techStack: [
-      techStack.find((tech) => tech.id === 2),
+      techStack.find((tech) => tech.id === 3),
       techStack.find((tech) => tech.id === 4),
       techStack.find((tech) => tech.id === 6),
       techStack.find((tech) => tech.id === 8),
@@ -37,10 +37,10 @@ const projects = [
   },
   {
     id: 3,
-    title: "Web Portfolio",
-    name: "Portfolio Bootstrap",
+    title: "Web",
+    name: "Upricing Music",
     status: "preview",
-    imgPath: "/assets/album/bs_portfolio.png",
+    imgPath: "/assets/album/uprising.png",
     url: "https://nathad17.github.io/bootstrap_portfolio/",
     techStack: [
       techStack.find((tech) => tech.id === 3),
@@ -53,7 +53,7 @@ const projects = [
     title: "Web Resep Makanan",
     name: "Dapur Kreatif",
     status: "comming soon",
-    imgPath: "/assets/album/dapur_kreatif.png",
+    imgPath: "/assets/album/Web2.png",
     url: "",
     techStack: [
       techStack.find((tech) => tech.id === 2),
@@ -68,7 +68,7 @@ const projects = [
     title: "Web Landing Page Sekolah",
     name: "PKBM Yeremia",
     status: "preview",
-    imgPath: "/assets/album/vue-school.png",
+    imgPath: "/assets/album/simpesa.png",
     url: "https://vue-school.vercel.app/",
     techStack: [
       techStack.find((tech) => tech.id === 2),
@@ -82,7 +82,7 @@ const projects = [
     title: "UI/UX Mobile Apps",
     name: "Flora Scan",
     status: "preview",
-    imgPath: "/assets/album/floraScan.png",
+    imgPath: "/assets/album/2.png",
     url: "https://rb.gy/ugcu2p",
     techStack: [techStack.find((tech) => tech.id === 1)].filter(Boolean),
     category: "UI/UX",
@@ -92,7 +92,7 @@ const projects = [
     title: "Web Link Tree",
     name: "Nath Links",
     status: "preview",
-    imgPath: "/assets/album/nath-links.png",
+    imgPath: "/assets/album/kpmi.png",
     url: "https://nath-links.vercel.app/",
     techStack: [
       techStack.find((tech) => tech.id === 2),
@@ -106,7 +106,21 @@ const projects = [
     title: "Web Landing Page Rental Mobil",
     name: "Nath Rental",
     status: "preview",
-    imgPath: "/assets/album/nath-rental.png",
+    imgPath: "/assets/album/posbindu.png",
+    url: "https://nath-rental.vercel.app/",
+    techStack: [
+      techStack.find((tech) => tech.id === 2),
+      techStack.find((tech) => tech.id === 5),
+      techStack.find((tech) => tech.id === 9),
+    ].filter(Boolean),
+    category: "FRONTEND",
+  },
+  {
+    id: 9,
+    title: "Web Landing Page Rental Mobil",
+    name: "Nath Rental",
+    status: "preview",
+    imgPath: "/assets/album/online.png",
     url: "https://nath-rental.vercel.app/",
     techStack: [
       techStack.find((tech) => tech.id === 2),
